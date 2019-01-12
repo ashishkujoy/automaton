1d23199bed9a9e7f3accb13b1d12b616f37c8695
Joy <ashishkujoy@gmail.com>
2019-01-12 15:49:05 +0530

    Remove state change for input validation

--- src/Dfa.js
+++ src/Dfa.js
@@ -1,9 +1,3 @@
-// states: ['q1', 'q2'],
-//     alphabets: ['1', '0'],
-//     delta: { q1: { '0': 'q2', '1': 'q1' }, q2: { '0': 'q1', '1': 'q2' } },
-//     'start-state': 'q1',
-//     'final-states': ['q2']
-
 class Dfa {
     constructor(states,currentState,finalStates,alphabets,delta) {
         this.states = states;
@@ -18,11 +12,11 @@ class Dfa {
     }
 
     doesAccept(string) {
-        string.split('').forEach(input => {
-            let nextState = this.delta[this.currentState][input];
-            this.currentState = nextState;
-        });
-        return this.finalStates.includes(this.currentState);
+        let finalState = string.split('').reduce((currentState,input) => {
+            return this.delta[currentState][input];
+        },this.currentState);
+
+        return this.finalStates.includes(finalState);
     }
 }
 
